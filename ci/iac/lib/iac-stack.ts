c06612490a34656d382800a3ed1e9277c714e363
quezlatch <quezlatch@gmail.com>
2020-03-22 21:24:27 +0000

    initial provisioning of codebuild

--- ci/iac/lib/iac-stack.ts
+++ ci/iac/lib/iac-stack.ts
@@ -1,9 +1,19 @@
 import * as cdk from '@aws-cdk/core';
+import * as codebuild from '@aws-cdk/aws-codebuild';
 
 export class IacStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        const project = new codebuild.Project(this, "Tatt", {
+            source: codebuild.Source.gitHub({
+                owner: 'quezlatch',
+                repo: 'try_all_the_things',
+                webhook: true, 
+                webhookFilters: [
+                    codebuild.FilterGroup.inEventOf(codebuild.EventAction.PUSH).andBranchIs('master'),
+                ]
+            })
+        })
+    }
 }
