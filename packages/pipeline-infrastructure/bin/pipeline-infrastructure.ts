78352366cfd8b73af474e900183ce9a53f49fb84
quezlatch <quezlatch@gmail.com>
2020-03-22 17:06:22 +0000

    try again

--- packages/pipeline-infrastructure/bin/pipeline-infrastructure.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import cdk = require('@aws-cdk/core');
-import { PipelineInfrastructureStack } from '../lib/pipeline-infrastructure-stack';
-
-const app = new cdk.App();
-new PipelineInfrastructureStack(app, 'PipelineInfrastructureStack');
